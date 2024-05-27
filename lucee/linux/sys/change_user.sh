1329264e4827cbbd98a09960ddcd19171a6dc61d
Kenric Ashe <26514157+kenricashe@users.noreply.github.com>
2024-05-26 19:14:15 -0700

    Add explanatory comment for new if block

--- lucee/linux/sys/change_user.sh
+++ lucee/linux/sys/change_user.sh
@@ -197,6 +197,7 @@ function rebuildControlScript {
         TEMP=`echo "" >> $TomcatControlScript`;
         TEMP=`echo "findpid() {" >> $TomcatControlScript`;
         TEMP=`echo "	PID_FOUND=0" >> $TomcatControlScript`;
+        TEMP=`echo "	# delete stale PID caused by server reboot" >> $TomcatControlScript`;
         TEMP=`echo "	if [[ /proc -nt \\"\\$CATALINA_PID\\" ]]; then" >> $TomcatControlScript`;
         TEMP=`echo "		rm \\"\\$CATALINA_PID\\"" >> $TomcatControlScript`;
         TEMP=`echo "	fi" >> $TomcatControlScript`;
