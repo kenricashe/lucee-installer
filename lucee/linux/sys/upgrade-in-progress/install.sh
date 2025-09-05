51881a335ff15a0b807d27d88d4de530051ef02c
Kenric Ashe <26514157+kenricashe@users.noreply.github.com>
2025-09-04 19:13:25 -0700

    chore: update install.sh version timestamp to 2025-09-04

--- lucee/linux/sys/upgrade-in-progress/install.sh
+++ lucee/linux/sys/upgrade-in-progress/install.sh
@@ -20,7 +20,7 @@ fi
 
 # UPDATE THIS WITH EACH COMMIT
 echo ""
-echo "install.sh version: 2025-08-25 13:48:08 Pacific"
+echo "install.sh version: 2025-09-04 19:13:07 Pacific"
 
 OWNER=${OWNER:-kenricashe}
 REPO=${REPO:-lucee-installer}
