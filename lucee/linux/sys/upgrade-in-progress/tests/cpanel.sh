cd5323d370dd6d46b472031d74a2ce9770d05e9b
Kenric Ashe <26514157+kenricashe@users.noreply.github.com>
2025-09-08 15:34:50 -0700

    feat: add support for copying lucee-proxy.conf to apache config directory

--- lucee/linux/sys/upgrade-in-progress/tests/cpanel.sh
+++ lucee/linux/sys/upgrade-in-progress/tests/cpanel.sh
@@ -69,6 +69,11 @@ create_httpd_conf() {
 		echo "" >> "$cpanel_config"
 		echo "IncludeOptional /etc/apache2/conf.d/*.conf" >> "$cpanel_config"
 		echo "" >> "$cpanel_config"
+
+		# Copy lucee-proxy.conf if exists
+		if [ -f "${CONF_DIR}/lucee-proxy.conf" ]; then
+			cp "${CONF_DIR}/lucee-proxy.conf" /etc/apache2/conf.d/
+		fi
 		
 		# Find and append VirtualHost blocks from other .conf files using discovery
 		echo "" >> "$cpanel_config"
