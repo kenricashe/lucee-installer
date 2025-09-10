19f775c40bdd89f6c2b7276d9f972f408ca70c8a
Kenric Ashe <26514157+kenricashe@users.noreply.github.com>
2025-09-10 09:38:56 -0700

    fix: remove ServerAlias mappings from vhost parsing to prevent duplicate entries

--- lucee/linux/sys/upgrade-in-progress/get-lucee-sites.sh
+++ lucee/linux/sys/upgrade-in-progress/get-lucee-sites.sh
@@ -184,15 +184,9 @@ parse_vhosts_file() {
 		/^[ \t]*#/ { next }
 		/<[ \t]*VirtualHost[> ]/ { in_vh=1; server=""; docroot=""; for (k in alias) delete alias[k]; ac=0; next }
 		in_vh==1 && /<[ \t]*\/[ \t]*VirtualHost[> ]/ {
-			# Emit ServerName mapping
+			# Only emit ServerName mapping, ignore ServerAlias to avoid duplicates
 			if (docroot != "" && server != "") {
 				printf "%s|%s|%s\n", server, docroot, vhost_file
-			}
-			# Also emit ServerAlias mappings for exclusion checking
-			for (i=1; i<=ac; i++) {
-				if (docroot != "" && alias[i] != "") {
-					printf "%s|%s|%s\n", alias[i], docroot, vhost_file
-				}
 			}
 			in_vh=0; server=""; docroot=""; for (k in alias) delete alias[k]; ac=0; next
 		}
--- lucee/linux/sys/upgrade-in-progress/version.sh
+++ lucee/linux/sys/upgrade-in-progress/version.sh
@@ -3,5 +3,5 @@
 # Centralized version information for upgrade-in-progress scripts
 # UPDATE THIS WITH EACH COMMIT
 
-VERSION_DATE="2025-09-10 09:29:27 Pacific"
+VERSION_DATE="2025-09-10 09:38:49 Pacific"
 VERSION_STRING="Version: ${VERSION_DATE}"
