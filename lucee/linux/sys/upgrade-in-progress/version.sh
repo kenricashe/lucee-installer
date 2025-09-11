3a6192770477a2ab5ed900af9d9a16c2e5c5a765
Kenric Ashe <26514157+kenricashe@users.noreply.github.com>
2025-09-11 16:12:39 -0700

    chore: update version timestamp to 2025-09-11 16:12:30 Pacific

--- lucee/linux/sys/upgrade-in-progress/version.sh
+++ lucee/linux/sys/upgrade-in-progress/version.sh
@@ -3,5 +3,5 @@
 # Centralized version information for upgrade-in-progress scripts
 # UPDATE THIS WITH EACH COMMIT
 
-VERSION_DATE="2025-09-11 02:26:47 Pacific"
+VERSION_DATE="2025-09-11 16:12:30 Pacific"
 VERSION_STRING="Version: ${VERSION_DATE}"
